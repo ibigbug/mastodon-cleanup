f4c3268d6fa0ea0990db79207cc867c3ce9a7dfe
Yuwei Ba <contact@yba.dev>
2024-05-20 05:21:56 +1000

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,8 @@ set -xe
 
 # https://ricard.dev/improving-mastodons-disk-usage/
 
-PATH=/opt/mastodon/bin/:$PATH
+PATH=/opt/mastodon/bin/:/opt/ruby/bin:$PATH
+export RAILS_ENV=production
 
 date
 
@@ -12,22 +13,22 @@ echo "instance version"
 tootctl --version
 
 echo "Prune remote accounts that never interacted with a local user"
-RAILS_ENV=production tootctl accounts prune;
+tootctl accounts prune;
 
 echo "Remove remote statuses that local users never interacted with older than 4 days"
-RAILS_ENV=production tootctl statuses remove --days 4;
+tootctl statuses remove --days 4;
 
 echo "Remove media attachments older than 4 days"
-RAILS_ENV=production tootctl media remove --days 4;
+tootctl media remove --days 4;
 
 # echo "Remove all headers (including people I follow)"
-# RAILS_ENV=production tootctl media remove --remove-headers --include-follows --days 0;
+# tootctl media remove --remove-headers --include-follows --days 0;
 
 echo "Remove link previews older than 4 days"
-RAILS_ENV=production tootctl preview_cards remove --days 4;
+tootctl preview_cards remove --days 4;
 
 echo "Remove files not linked to any post"
-RAILS_ENV=production tootctl media remove-orphans;
+tootctl media remove-orphans;
 
 echo "Clean up done"
 date
